9d0982fd7a51777293dc7afd508773e60f0941da
David Ant <145033515+David-Ant@users.noreply.github.com>
2025-08-04 14:44:47 +0100

    vidual update for customSetup

--- src/app/(pages)/customSetup/page.tsx
+++ src/app/(pages)/customSetup/page.tsx
@@ -15,7 +15,7 @@ export default function ProductPage() {
     const [wheels, setWheels] = useState<stockItem | undefined>(undefined);
     const [deckModalOpen, setDeckModalOpen] = useState(false);
     const [wheelsModalOpen, setWheelsModalOpen] = useState(false);
-    
+
     const { data: allStock, isLoading, error } = api.stock.getStock.useQuery();
     const deckStock = allStock?.filter((item) => item.type === "DECK");
     const wheelStock = allStock?.filter((item) => item.type === "WHEEL");
@@ -37,36 +37,38 @@ export default function ProductPage() {
     }
 
     return (
-        <main>
-            <h2 className="p-3 pl-10">
-                Make your own custom skateboard
-            </h2>
-            <div className="flex justify-between pl-10 pr-10 pt-3">
-                <div className="flex flex-col items-start justify-start max-w-[260px]">
+        <main className="flex flex-col items-center justify-center w-full h-full p-4 bg-linear-to-b from-amber-200 to-gray-200">
+            <div className="flex-row items-center justify-center max-w-[50%] pb-4">
+                <h2 className="pb-5 items-center justify-center !text-5xl font-bold">
+                    Make your own custom skateboard
+                </h2>
+                <div className="flex flex-col items-start justify-start max-w-[260px] pl-6">
                     <i>Pick a deck</i>
                     <i>Pick a set of wheels</i>
                     <i>We assemble it for you</i>
                 </div>
-                <div className="flex flex-col items-start justify-start max-w-[260px] pl-12">
+            </div>
+            <div className="flex items-start justify-between px-10 py-3 space-x-12">
+                <div className={`flex flex-col items-start justify-start max-w-[260px] rounded-[10px] ${deck ? "bg-white border border-gray-300" : "bg-[#e0e0e0]"} shadow-md`}>
                     <button
                         onClick={() => setDeckModalOpen(true)}
-                        className="w-[250px] h-[250px] flex items-center justify-center rounded-[10px] bg-[#e0e0e0] text-[1rem] text-[#333] font-bold cursor-pointer overflow-hidden transition-colors duration-200 ease-in-out shadow"
+                        className="w-[250px] h-[250px] flex items-center justify-center text-[1rem] text-[#333] font-bold cursor-pointer"
                     >
                         {deck ? (
                             <div className="relative w-[90%] h-[90%]">
-                            <Image
-                                src={deck.imageUrl}
+                                <Image
+                                    src={deck.imageUrl}
                                     fill
                                     className="rounded object-cover"
-                                alt={`Deck: ${deck.name}`}
-                            />
+                                    alt={`Deck: ${deck.name}`}
+                                />
                             </div>
                         ) : (
                             <p>Select Deck</p>
                         )}
                     </button>
                     {deck ? (
-                        <div>
+                        <div className="p-4">
                             <h2>{deck.name}</h2>
                             <p>Brand: {deck.brand}</p>
                             <p>Width: {deck.width + '"'}</p>
@@ -76,26 +78,26 @@ export default function ProductPage() {
                         <p></p>
                     )}
                 </div>
-                <div className="flex flex-col items-start justify-start max-w-[260px] pr-12">
+                <div className={`flex flex-col items-start justify-start max-w-[260px] rounded-[10px] ${wheels ? "bg-white border border-gray-300" : "bg-[#e0e0e0]"} shadow-md`}>
                     <button
                         onClick={() => setWheelsModalOpen(true)}
-                        className="w-[250px] h-[250px] flex items-center justify-center rounded-[10px] bg-[#e0e0e0] text-[1rem] text-[#333] font-bold cursor-pointer overflow-hidden transition-colors duration-200 ease-in-out shadow">
+                        className="w-[250px] h-[250px] flex items-center justify-center text-[1rem] text-[#333] font-bold cursor-pointer">
 
                         {wheels ? (
                             <div className="relative w-[90%] h-[90%]">
-                            <Image
-                                src={wheels.imageUrl}
+                                <Image
+                                    src={wheels.imageUrl}
                                     fill
                                     className="rounded object-cover"
-                                alt={`Wheels: ${wheels.name}`}
-                            />
+                                    alt={`Wheels: ${wheels.name}`}
+                                />
                             </div>
                         ) : (
                             <p>Select Wheels</p>
                         )}
                     </button>
                     {wheels ? (
-                        <div>
+                        <div className="p-4">
                             <h2>{wheels.name}</h2>
                             <p>Brand: {wheels.brand}</p>
                             <p>Durometer: {wheels.durometer}</p>
@@ -105,22 +107,29 @@ export default function ProductPage() {
                         <p></p>
                     )}
                 </div>
-                <div>
+                <div className="w-[180px] space-y-4">
                     <div className={wheels && deck ?
-                        "w-[140px] h-[40px] flex items-center justify-center bg-orange-500 border border-white/30 rounded-lg font-semibold text-[16px] text-white cursor-pointer shadow"
-                        : "w-[140px] h-[40px] flex items-center justify-center bg-gray-500 border border-black/10 rounded-lg font-semibold text-[16px] text-white shadow-inner"}>
+                        "h-[40px] flex items-center justify-center bg-orange-500 border border-white/30 rounded-lg font-semibold text-[16px] text-white cursor-pointer shadow-md"
+                        : "h-[40px] flex items-center justify-center bg-gray-500 border border-black/10 rounded-lg font-semibold text-[16px] text-white shadow-inner"}>
                         <CustomOrderButton
                             deckId={deck ? deck.id : ""}
                             wheelsId={wheels ? wheels.id : ""}
                         />
                     </div>
                     {wheels && deck && (
-                        <p>Cost: {(Math.round((deck.cost + wheels.cost + 5) * 100) / 100)}€</p>
+                        <div className="bg-white/80 p-4 rounded-lg shadow-md border border-gray-300">
+                            <p className="font-bold mb-2">Cost</p>
+                            <p>Deck: {deck.cost}€</p>
+                            <p>Wheels: {wheels.cost}€</p>
+                            <p>Assembly: 5€</p>
+                            <hr className="my-2" />
+                            <p className="font-semibold">Total: {(Math.round((deck.cost + wheels.cost + 5) * 100) / 100)}€</p>
+                        </div>
                     )}
                 </div>
             </div>
 
-            <div className="w-1/2 p-4 bg-gray-200">
+            <div className="w-1/2 p-4 bg-none">
                 <ProductPickerModal
                     open={deckModalOpen}
                     onClose={() => setDeckModalOpen(false)}
@@ -129,7 +138,7 @@ export default function ProductPage() {
                     handleStockSubmit={handleDeckSubmit}
                 />
             </div>
-            <div className="w-1/2 p-4 bg-gray-200">
+            <div className="w-1/2 p-4 bg-none">
                 <ProductPickerModal
                     open={wheelsModalOpen}
                     onClose={() => setWheelsModalOpen(false)}
